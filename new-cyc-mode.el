9fa8589884f06aabaaae7992a873bbe9a3c37376
Andrew Dougherty <andrewdo@frdcsa.org>
2014-06-23 19:51:15 -0500

    Cleanup.

--- new-cyc-mode.el
+++ new-cyc-mode.el
@@ -1,10 +1,10 @@
 (global-set-key "\C-ccc" 'cm-get-comment-for-constant)
-
+   
 (defun cm-get-comment-for-constant ()
  "Return the comment for a constant"
  (interactive)
  (let*
   ((constant (thing-at-point 'symbol)))
   (message constant)
-  ;   (uea-send-contents (concat "OpenCyc, (comment " constant ")"))
+  ;; (uea-send-contents (concat "OpenCyc, (comment " constant ")"))
   ))
